4b3d07e67e2a23afc1a3dce760e5cb8f2572fde4
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-23 22:15:09 +0530

    commit

--- Code_Asylums/dsa_bootcamp/dailyclasses/day5/longestPath.py
+++ Code_Asylums/dsa_bootcamp/dailyclasses/day5/longestPath.py
@@ -1,5 +1,10 @@
 def longestPath(graph, source):
-    visited={}
+    elements={}
+    parents=[source]
+    while parents:
+        for i in parents:
+            if 
+
 
 graph={3:[2,4,6,7], 2:[1], 1:[], 4:[5], 5:[], 6:[],7:[8], 8:[9], 9:[]}
 print(longestPath(graph, 3))
