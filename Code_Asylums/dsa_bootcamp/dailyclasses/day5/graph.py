db191d8d0d37c844d05eb991742eddf132cfadb3
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-23 20:56:35 +0530

    commit

--- Code_Asylums/dsa_bootcamp/dailyclasses/day5/graph.py
+++ Code_Asylums/dsa_bootcamp/dailyclasses/day5/graph.py
@@ -4,9 +4,8 @@ class graph:
         self.vertices=v
     
     def ratTraverse(self, posX, posY, grid, visited, destX=3, destY=3):
-        print(grid[posY][posX])
+        print(posX, posY)
         if posX>=len(grid[-1]) or posY>=len(grid):
-            print("here")
             return False
 
         if visited[posY][posX]==1:
@@ -14,21 +13,18 @@ class graph:
 
         visited[posY][posX]=1
         if destX==posX and destY==posY:
-            print("possible")
             return True
         
         if grid[posY][posX]==0:
-            print("there")
             return False
 
-        r=self.ratTraverse(posX+1, posY, grid, destX, destY)
-        d=self.ratTraverse(posX, posY+1, grid, destX, destY)
-        l=self.ratTraverse(posX-1, posY, grid, destX, destY)
-        u=self.ratTraverse(posX, posY-1, grid, destX, destY)
+        r=self.ratTraverse(posX+1, posY, grid, visited, destX, destY)
+        d=self.ratTraverse(posX, posY+1, grid, visited, destX, destY)
+        l=self.ratTraverse(posX-1, posY, grid, visited, destX, destY)
+        u=self.ratTraverse(posX, posY-1, grid, visited, destX, destY)
 
-        if u or d or l or u:
-            print("not possible")
-            return False
+        return r or d or l or u
+        
             
 
 g=graph()
@@ -39,11 +35,11 @@ grid=[
 [1,1,1,1]
 ]
 visited=[
-[1,0,1,0],
-[1,1,0,0],
-[1,0,1,0],
-[1,1,1,1]
+[0,0,0,0],
+[0,0,0,0],
+[0,0,0,0],
+[0,0,0,0]
 ]
-print(g.ratTraverse(0, 0, grid, visited, -1, -1))
 
-    
+print(g.ratTraverse(0, 0, grid, visited, 3, 3))
+
