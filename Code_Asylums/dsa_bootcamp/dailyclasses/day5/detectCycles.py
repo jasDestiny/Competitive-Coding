935f050f7367c51a1467eb57c2c8d3c654265737
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-23 22:07:49 +0530

    commit

--- Code_Asylums/dsa_bootcamp/dailyclasses/day5/detectCycles.py
+++ Code_Asylums/dsa_bootcamp/dailyclasses/day5/detectCycles.py
@@ -7,15 +7,15 @@ def detectCycle(adjacencyMatrix, source):
             if i in visited:
                 return "CYCLE DETECTED"
             visited[i]=True
-            for j in adjacencyMatrix[i]:
-                if adjacencyMatrix[j]==1:
-                    children.append(j)
+            for i,j in enumerate(adjacencyMatrix[i]):
+                if j==1:
+                    children.append(i)
         parents=children.copy()
     return "NO CYCLES"
                     
 
 adjacencyMatrix=[
-    [0, 1, 1],
+    [0, 0, 1],
     [1, 0, 0],
     [1, 0, 0]
 ]
--- Code_Asylums/dsa_bootcamp/dailyclasses/day5/longestPath.py
+++ Code_Asylums/dsa_bootcamp/dailyclasses/day5/longestPath.py
@@ -1,6 +1,5 @@
 def longestPath(graph, source):
     visited={}
 
-
 graph={3:[2,4,6,7], 2:[1], 1:[], 4:[5], 5:[], 6:[],7:[8], 8:[9], 9:[]}
 print(longestPath(graph, 3))
