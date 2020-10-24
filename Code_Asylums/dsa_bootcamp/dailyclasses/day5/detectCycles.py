7d3f6fd00269abe8804fa3d6f8f22c1efc921372
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-24 20:45:54 +0530

    commit

--- Code_Asylums/dsa_bootcamp/dailyclasses/day5/detectCycles.py
+++ Code_Asylums/dsa_bootcamp/dailyclasses/day5/detectCycles.py
@@ -1,23 +1,17 @@
-def detectCycle(adjacencyMatrix, source):
-    parents=[source]
-    visited={}
-    while parents:
-        children=[]
-        for i in parents:
-            print(i)
-            if i in visited:
-                return "CYCLE DETECTED"
-            visited[i]=True
-            for k,j in enumerate(adjacencyMatrix[i]):
-                if j==1 and k!=i:
-                    children.append(k)
-        parents=children.copy()
-    return "NO CYCLES"
-                    
+def detectCycle(adjacencyMatrix, current, visited, parent=None):
+    if visited[current]:
+        return True
+    visited[current]=True
+    for i,j in enumerate(adjacencyMatrix[current]):
+        if j and i!=parent:
+            if detectCycle(adjacencyMatrix, i, visited, current):
+                return True
+    return False
 
 adjacencyMatrix=[
-    [0, 1, 1],
-    [1, 0, 1],
+    [0, 1, 0],
+    [0, 0, 1],
     [0, 1, 0]
 ]
-print(detectCycle(adjacencyMatrix, 0))
+n=3
+print(detectCycle(adjacencyMatrix, 0, [False]*n))
