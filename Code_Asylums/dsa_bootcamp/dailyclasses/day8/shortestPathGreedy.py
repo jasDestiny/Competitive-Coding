cc892d7f72e6bf063d100fe3f865c0e507c88303
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-31 22:03:42 +0530

    commit

--- Code_Asylums/dsa_bootcamp/dailyclasses/day8/shortestPathGreedy.py
+++ Code_Asylums/dsa_bootcamp/dailyclasses/day8/shortestPathGreedy.py
@@ -1,10 +1,12 @@
 def shortestPath(map, source, target):
     greedy=[float("inf") for i in range(len(map))]
+    visited=[False for i in range(len(map))]
     greedy[source]=0
+    visited[source]=True
     parent=[source]
     current=parent
     while parent:
+        children=[]
         for i in parent:
             for j,k in enumerate(map[i]):
                 if k!=float("-inf"):
-                    
