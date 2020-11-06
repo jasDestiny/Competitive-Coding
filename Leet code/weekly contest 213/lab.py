8e654f2f430b64c9026c13d682f681d0d01a3e12
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-11-06 17:53:33 +0530

    commit

--- Leet code/weekly contest 213/lab.py
+++ Leet code/weekly contest 213/lab.py
@@ -1,18 +1,18 @@
 def findShortestPath(graph, source, target):
-    table, visited=[float("inf") for i in range(len(graph))], [False for i in range(len(graph))]
+    table, visited=[float("inf") for i in graph], [False for i in graph]
     current, currentDist=source, 0
     table[current], visited[current]=0, True
     for i in range(len(graph)):
-        for i in range(0, len(graph[current], 1)):
+        for i in range(0, len(graph[current]), 1):
             if graph[current][i]!=float("inf") and not visited[i]:
                 table[i]=min(table[i], map[current][i]+currentDist)
-        minVal, min=float("inf"), None
+        minVal, mini=float("inf"), None
         for i in range(len(table)):
-            if table[i]<minVal and not Visited[i]:
+            if table[i]<minVal and not visited[i]:
                 visited[i]=True
                 minVal=table[i]
-                min=i
-        if min==target:
+                mini=i
+        if mini==target:
             break
         current=min
         currentDist=minVal
@@ -20,5 +20,5 @@ def findShortestPath(graph, source, target):
 
 n=float("inf")
 graph=[[0, 3, n, 7, 8],[3, 0, 1, 4, n],[n, 1, 0, 2, n],[7, 4, 2, 0, 4],[8, n, n, 3, 0]]
-print(findShortestPath(n, 0, 1))
+print(findShortestPath(graph, 0, 1))
 
